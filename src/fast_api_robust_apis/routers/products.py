bf6c32ba2f6d6920b6306ae16d3e76cf076ed9e4
Hugo Arevalo <gohulu@gmail.com>
2026-09-04 08:41:24 -0500

    allow creating new products

--- src/fast_api_robust_apis/routers/products.py
+++ src/fast_api_robust_apis/routers/products.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from fast_api_robust_apis.db.session import get_db
 from fast_api_robust_apis.crud.product import product_repository
+from fast_api_robust_apis.schemas.product import ProductCreate
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
@@ -14,3 +15,15 @@ def list_products(
     if search:
         return product_repository.search(db, search, skip=skip, limit=limit)
     return product_repository.get_all(db, skip=skip, limit=limit)
+
+
+@router.post("/")
+def create_product(product: ProductCreate, db: Session = Depends(get_db)):
+    """Create a new product"""
+    return product_repository.create(
+        db,
+        name=product.name,
+        description=product.description,
+        sku=product.sku,
+        price=product.price,
+    )
